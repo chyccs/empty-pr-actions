64356c29faf2c4aa9bcb9ed3c72c78673e57dd79
chyccs <chyccs@naver.com>
2023-03-29 19:13:32 +0900

    aaaa

--- src/pull-request.sh
+++ src/pull-request.sh
@@ -27,21 +27,16 @@ then
     contents=$(echo "$issue_title_converted" | sed 's/\(.*\)\:\(.*\)/\2/g')
     pull_request_title=$(echo "$tag: #$number $contents")
 fi
+
 echo "::debug::head_branch=$head_branch"
 echo "::debug::pull_request_title=$pull_request_title"
 
-# list=$(gh issue develop -l "$number")
-# echo "::debug::list=$list"
-gh issue develop -l "$number"
+gh issue develop -c "$number" --name "$head_branch" --base "$base_branch" --repo "$repo"
 result=$?
-echo "::debug::gh issue develop -l $number => $result"
-if [ $result -eq 1 ]
+echo "::debug::gh issue develop -c $number --name "$head_branch" --base $base_branch --repo $repo => $result"
+
+if [ $result -eq 0 ]
 then
-    gh issue develop -c "$number" --name "$head_branch" --base "$base_branch" --repo "$repo"
-    echo "::debug::gh issue develop -c $number --name $head_branch --base $base_branch --repo $repo"
-    
-    git checkout "$head_branch"
-    echo "::debug::git checkout $head_branch"
     git config --local user.email "$email"
     git config --local user.name "$owner"
     git commit --allow-empty -m "trigger notification\n[skip ci]"
@@ -49,8 +44,11 @@ then
     git push --set-upstream origin "$head_branch"
     echo "::debug::git push --set-upstream origin $head_branch"
 
-    gh pr create --title "$pull_request_title" --body " " --repo "$repo" --base "$base_branch" --head "$head_branch"
+    new_pr=$(gh pr create --title "$pull_request_title" --repo "$repo" --base "$base_branch" --head "$head_branch" --body-file ".github/pull_request_template.md")
     echo "::debug::gh pr create --title $pull_request_title --body ' ' --repo $repo --base $base_branch --head $head_branch"
+
+    gh issue comment $number --body "Pull Request created. $new_pr" --repo $repo
 else
     echo "::debug::branch is already created"
+    gh issue comment $number --body "Can not create Pull Request. Branch is already exists" --repo $repo
 fi
